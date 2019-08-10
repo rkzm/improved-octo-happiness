a32845ccf7e423922a2a383239600a13397b6249
rex <rkzm@github.com>
2019-08-10 23:33:28 +1000

    add 404

--- application.rb
+++ application.rb
@@ -6,6 +6,11 @@ class Application
     # content or body (multiple arrays)
     if env['PATH_INFO'] == '/users'
       [200, {}, [Database.users.to_s]]
+    elsif env['PATH_INFO'] =~ %r{/users/\d+}
+      id = env['PATH_INFO'].split('/').last.to_i
+      [200, {}, [Database.users[id].to_s]]
+    else
+      [404, {}, ['Nothing here']]
     end
   end
 end
