05b4bfb44bb98d1734024db46ee1f5f03be476be
rex <rkzm@github.com>
2019-08-11 10:52:33 +1000

    set correct Content-Type

--- users_application.rb
+++ users_application.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
+require 'json'
 # web server invokes the call method
 class UsersApplication
   def call(env)
     response = Rack::Response.new
+    response.headers['Content-Type'] = 'application/json'
     # status code
     # hash with response to browser
     # content or body (multiple arrays)
     if env['PATH_INFO'] == ''
-      response.write(Database.users.to_s)
+      response.write(JSON.generate(Database.users.to_s))
     elsif env['PATH_INFO'] =~ %r{/\d+}
       id = env['PATH_INFO'].split('/').last.to_i
-      response.write(Database.users[id].to_s)
+      response.write(JSON.generate(Database.users[id].to_s))
     else
       response.status = 404
       response.write('Nothing Here!')
