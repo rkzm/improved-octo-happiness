44e4875de5b37cffb81bb58917b7e2566d3e37d6
rex <rkzm@github.com>
2019-08-11 10:45:14 +1000

    using response... no more issues with headers response codes are handled for us all we do is write to the body

--- users_application.rb
+++ users_application.rb
@@ -3,16 +3,19 @@
 # web server invokes the call method
 class UsersApplication
   def call(env)
+    response = Rack::Response.new
     # status code
     # hash with response to browser
     # content or body (multiple arrays)
     if env['PATH_INFO'] == ''
-      [200, {}, [Database.users.to_s]]
+      response.write(Database.users.to_s)
     elsif env['PATH_INFO'] =~ %r{/\d+}
       id = env['PATH_INFO'].split('/').last.to_i
-      [200, {}, [Database.users[id].to_s]]
+      response.write(Database.users[id].to_s)
     else
-      [404, {}, ['Nothing here']]
+      response.status = 404
+      response.write('Nothing Here!')
     end
+    response.finish
   end
 end
